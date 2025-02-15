93962621677fa1e8be01c520c0aff689a64ae72c
bunny1911 <artur.luk2001@gmail.com>
2025-02-15 18:56:49 +0200

    Added function for get list of receipts

--- app/funcs/receipt/funcs.py
+++ app/funcs/receipt/funcs.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 
+from datetime import datetime
 from fastapi import HTTPException
 
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -112,3 +113,77 @@ async def get_receipt(
         )
 
     return receipt
+
+
+async def get_receipts(
+    db_session: AsyncSession,
+    user_id: int,
+    start_date: datetime | None = None,
+    end_date: datetime | None = None,
+    total: float | None = None,
+    payment_type: str | None = None,
+    page: int = 0,
+    on_page: int = 10
+) -> dict:
+    """
+    Function to retrieve a list of receipts for a user, applying filters and pagination.
+    We first fetch all receipts for the user, apply filters, and then calculate totals and pagination.
+
+    Args:
+        db_session (AsyncSession): The database session.
+        user_id (int): The ID of the user whose receipts we are fetching.
+        start_date (datetime | None): Filter receipts by start date.
+        end_date (datetime | None): Filter receipts by end date.
+        total (float | None): Filter receipts with a total greater than or equal to the given value.
+        payment_type (str | None): Filter by payment type (cash or card).
+        page (int): The page number for pagination.
+        on_page (int): The number of records per page.
+
+    Returns:
+        dict: The filtered and paginated list of receipts with total calculations.
+    """
+
+    # Create a base query for receipts
+    query = select(
+        Receipt
+    ).filter(
+        Receipt.user_id == user_id
+    )
+
+    # Filters
+    if start_date:
+        # Filter by start data
+        query = query.filter(Receipt.created_at >= start_date)
+
+    if end_date:
+        # Filter by end data
+        query = query.filter(Receipt.created_at <= end_date)
+
+    if total is not None:
+        # Filter by total price of receipt
+        query = query.filter(Receipt.total >= total)
+
+    if payment_type:
+        # # Filter by payment type
+        query = query.join(Receipt.payment_method).filter(Receipt.payment_method.title == payment_type)
+
+    # Apply pagination
+    query = query.offset((page - 1) * on_page).limit(on_page)
+
+    # Defined receipts with pagination and filters
+    results = await db_session.execute(query)
+    receipts = results.scalars().all()
+
+    # Defined total count of results
+    total = len(receipts)
+
+    # Defined next page
+    next_page = page + 1 if len(receipts) == on_page else None
+
+    return {
+        "total": total,
+        "page": page,
+        "on_page": on_page,
+        "next_page": next_page,
+        "results": receipts
+    }
